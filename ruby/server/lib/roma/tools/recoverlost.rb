f42461416bf22431d389f165fece5a2d99dff4f4
junji torii <jun930@gmail.com>
2010-01-14 19:34:01 +0900

    remove files

--- ruby/server/lib/roma/tools/recoverlost.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-# -*- coding: utf-8 -*-
-#
-# usage:recoverlost address port storage-path [yyyymmddhhmmss]
-#
-require 'roma/tools/recoverlost_lib'
-
-Roma::RecoverLost.new('recoverlost', 'spushv', ARGV).suite
-puts "Recover process has succeed."
